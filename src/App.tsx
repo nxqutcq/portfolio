7caa3a8e253aa48ad78fd9379d0dd1c1c37e39c5
vlad <vnelipovich63@gmail.com>
2023-06-29 04:01:46 +0300

    did 'contacts' component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {Header} from "./header/Header";
 import {Main} from "./main/Main";
 import {Skills} from "./skills/Skills";
 import {Projects} from "./projects/Projects";
-import {RemoteWork} from "./remoteWork/RemoteWork";
 import {Contacts} from "./contacts/Contacts";
 import {Footer} from "./footer/Footer";
 
@@ -15,7 +14,6 @@ function App() {
             <Main/>
             <Skills/>
             <Projects/>
-            <RemoteWork/>
             <Contacts/>
             <Footer/>
         </div>
--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,10 +1,17 @@
 import styleContainer from '../common/styles/Container.module.css'
 import style from '../contacts/Contacts.module.css'
+
 export const Contacts = () => {
     return (
-        <div className={styleContainer.container}>
-            <div className={style.containerContacts}>
+        <div className={style.contactsBlock}>
+            <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <h2>Contacts</h2>
+                    <form action="" className={style.form}>
+                        <input type="text" className={style.firstInput}/>
+                        <input type="text" className={style.secondInput}/>
+                        <textarea name="" id="" className={style.textarea}></textarea>
+                    </form>
+                    <button>Send</button>
             </div>
         </div>
     )
