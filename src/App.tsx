13548372e3cfa6beec8eeac82a302003e3c267b9
vlad <vnelipovich63@gmail.com>
2024-07-03 04:35:19 +0300

    fix: styles

--- src/App.tsx
+++ src/App.tsx
@@ -105,9 +105,9 @@ export const App = () => {
               <g
                 id="Page-1"
                 stroke="none"
-                stroke-width="1"
+                strokeWidth="1"
                 fill="none"
-                fill-rule="evenodd"
+                fillRule="evenodd"
               >
                 <g
                   id="Dribbble-Light-Preview"
--- src/Skills.tsx
+++ src/Skills.tsx
@@ -22,7 +22,7 @@ export const Skills = () => (
             />
           </svg>
         </div>
-        <span>React</span>
+        <span className="mt-2">React</span>
       </div>
       <div className="skill-container">
         <div className="skill-icon">
@@ -39,7 +39,7 @@ export const Skills = () => (
             />
           </svg>
         </div>
-        <span>Redux</span>
+        <span className="mt-2">Redux</span>
       </div>
       <div className="skill-container">
         <div className="skill-icon">
@@ -72,7 +72,7 @@ export const Skills = () => (
             </g>
           </svg>
         </div>
-        <span>React Query</span>
+        <span className="mt-2">React Query</span>
       </div>
       <div className="skill-container">
         <div className="skill-icon">
@@ -104,7 +104,7 @@ export const Skills = () => (
             </g>
           </svg>
         </div>
-        <span>TypeScript</span>
+        <span className="mt-2">TypeScript</span>
       </div>
       <div className="skill-container">
         <div className="skill-icon">
@@ -123,7 +123,7 @@ export const Skills = () => (
             />
           </svg>
         </div>
-        <span>Sass</span>
+        <span className="mt-2">Sass</span>
       </div>
       <div className="skill-container">
         <div className="skill-icon">
@@ -142,7 +142,34 @@ export const Skills = () => (
             />
           </svg>
         </div>
-        <span>Tailwind CSS</span>
+        <span className="mt-2">Tailwind CSS</span>
+      </div>
+      <div className="skill-container">
+        <div className="skill-icon">
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle cx="16" cy="16" r="14" fill="#1794D4" />
+            <path d="M18 7H16V9H18V7Z" fill="white" />
+            <path d="M10 10H12V12H10V10Z" fill="white" />
+            <path
+              d="M6.00155 16.9414C6.17244 19.8427 7.90027 24 14 24C20.8 24 23.8333 19 24.5 16.5C25.3333 16.5 27.2 16 28 14C27.5 13.5 25.5 13.5 24.5 14C24.5 13.2 24 11.5 23 11C22.3333 11.6667 21.3 13.4 22.5 15C22 16 20.6667 16 20 16H6.9429C6.41342 16 5.97041 16.4128 6.00155 16.9414Z"
+              fill="white"
+            />
+            <path d="M9 13H7V15H9V13Z" fill="white" />
+            <path d="M10 13H12V15H10V13Z" fill="white" />
+            <path d="M15 13H13V15H15V13Z" fill="white" />
+            <path d="M16 13H18V15H16V13Z" fill="white" />
+            <path d="M21 13H19V15H21V13Z" fill="white" />
+            <path d="M15 10H13V12H15V10Z" fill="white" />
+            <path d="M16 10H18V12H16V10Z" fill="white" />
+          </svg>
+        </div>
+        <span className="mt-2">Docker</span>
       </div>
     </div>
   </div>
