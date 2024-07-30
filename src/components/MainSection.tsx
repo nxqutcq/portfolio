b6ad255236c78c226cd68f1c49b83e2db92a744d
vlad <vnelipovich63@gmail.com>
2024-07-30 10:23:11 +0300

    fix: main section

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -6,19 +6,21 @@ const MainSection: React.FC<{ scrollRefs: ScrollRefs }> = ({ scrollRefs }) => (
     <header className="z-50 bg-black/60 py-2 fixed flex flex-col w-screen items-center">
       <Navbar scrollRefs={scrollRefs} />
     </header>
-    <div className="flex flex-row w-full xs1:gap-x-[20%] px-5 xs:gap-x-[5%] items-center container justify-center mt-[13rem]">
-      <div className="h-fit flex gap-4 animate-float-up flex-col">
-        <span className="animate-pulse">Hi there!</span>
-        <h1 className="text-2xl">I'm Vladislav Nelipovich</h1>
-        <span>Frontend Developer</span>
-      </div>
-      <div>
-        <div className="max-w-[13rem] animate-float-up overflow-hidden rounded-full">
-          <img
-            draggable={false}
-            src="/src/assets/photos/oAT8xJ6LLDQ.webp"
-            alt="my-photo"
-          />
+    <div className="h-full w-full items-center flex flex-col justify-center">
+      <div className="flex flex-row w-full xs1:gap-x-[20%] px-5 xs:gap-x-[5%] items-center container justify-center">
+        <div className="h-fit flex gap-4 animate-float-up flex-col">
+          <span className="animate-pulse">Hi there!</span>
+          <h1 className="text-2xl">I'm Vladislav Nelipovich</h1>
+          <span>Frontend Developer</span>
+        </div>
+        <div>
+          <div className="max-w-[13rem] animate-float-up overflow-hidden rounded-full">
+            <img
+              draggable={false}
+              src="/src/assets/photos/oAT8xJ6LLDQ.webp"
+              alt="my-photo"
+            />
+          </div>
         </div>
       </div>
     </div>
