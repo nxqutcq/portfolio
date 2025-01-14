cbf81154f31bcd549181b7ee6c9d756a306cbd15
vlad <vnelipovich63@gmail.com>
2025-01-14 20:03:06 +0300

    feat: add styles for mail btn

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,6 +1,48 @@
-const Contacts = () => (
-  <a className="p-5 border rounded-md" href="mailto:vnelipovich63@gmail.com">
-    Say hello
-  </a>
-)
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+const Contacts = () => {
+  const buttonRef = useRef(null)
+
+  const handleMouseEnter = () => {
+    gsap.to(buttonRef.current, {
+      backgroundSize: '100% 100%',
+      duration: 0.5,
+      ease: 'power2.out',
+    })
+  }
+
+  const handleMouseLeave = () => {
+    gsap.to(buttonRef.current, {
+      backgroundSize: '100% 0%',
+      duration: 0.5,
+      ease: 'power2.out',
+    })
+  }
+  
+  const handleClick = () => {
+    window.location.href = 'mailto:vnelipovich63@gmail.com'
+  }
+
+  return (
+    <button
+      ref={buttonRef}
+      onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="px-5 py-4 border rounded-md"
+      style={{
+        cursor: 'pointer',
+        backgroundImage: 'linear-gradient(to top, #07c98863, #09771755)',
+        backgroundSize: '100% 0%',
+        backgroundRepeat: 'no-repeat',
+        color: 'white',
+        borderRadius: '5px',
+        fontSize: '16px',
+        transition: 'color 0.5s',
+      }}
+    >
+      Say hello
+    </button>
+  )
+}
 export default Contacts
